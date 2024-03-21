8892bb75a401a24b7824d656c3b3972180548620
ItsTheChickenMan <pxdallacosta@gmail.com>
2024-03-20 21:38:39 -0400

    added hyde

---
+++ egghunt/hyde/encoded.js
@@ -0,0 +1,2 @@
+const encoded = 'X\x7Fd edpa\'%bbe&}a%ga#".Lf$qdu)tn\'{{jdxmfl odp/+yki zakwd"nu.whi(bouv)j`eaoibonnh%ha`%|it$)"Co"majgibo%bced#(eao$cn%tij)f}f}mey}"ndgfu,# F}y hk{z| %kivm,s!yah+ch|t&ycwt"dh.whi(`e{<))m|vxmwaye##)#Tdx$bk}vjr"gg.zrm|nni"}omwj#hc\x7Fc.!%Ha\x7Fd#xhk)nfsv%gej!$(mtvr|1\'#iumzm#g`d`o`#fcm%mtnvg$hmodizx/3I1wb=D]O`AN1V\x7F<OpVDIb~^/<GriMJEk';
+const keyLength = 37;
